c4bf0440434e432f3310534a7567bde39fd5ae05
Nasser <abdounasser202@gmail.com>
2024-09-08 18:43:36 +0200

    Counter App

--- rustle/rudock/src/main.rs
+++ rustle/rudock/src/main.rs
@@ -1,60 +1,57 @@
-use iced::widget::button;
-use iced::widget::text;
-use iced::widget::column;
+use iced::widget::{button, column, text};
+use iced::{Element, Sandbox, Settings};
 
+// state that represents the data
 struct Counter {
-       value: i64,
+     value: i64,
 }
 
+// message that represent the event
 #[derive(Debug, Clone, Copy)]
 enum Message {
      Increment,
      Decrement,
 }
 
-impl Counter {
+// The sandbox that contains the application logic
+impl Sandbox for Counter {
+    type Message = Message;
 
-     fn update(&mut self, message:Message) {
-     	match message {
-	      Message::Increment => {
-	      	self.value += 1;
-	      }
-	      Message::Decrement => {
-	      	self.value -= 1;
-	      }
-	}
+     // Constructor
+     fn new() -> Self {
+          Self { value: 0 }
+     }
+
+     // define title of the app    
+     fn title(&self) -> String {
+        String::from("Counter")
+     }
+
+     // to handle state changes based on Message
+     // the app logic lives here 
+     fn update(&mut self, message: Message) {
+          match message {
+               Message::Increment => {
+                    self.value += 1;
+               }
+               Message::Decrement => {
+                    self.value -= 1;
+               }
+          }
+     }
+     
+     // describe how to display the UI
+     fn view(&self) -> Element<Message> {
+          let increment = button("+").on_press(Message::Increment);
+          let decrement = button("-").on_press(Message::Decrement);
+          let content = text(self.value.to_string()).size(40);
+          column![increment, content, decrement]
+               .padding(20)
+               .align_items(iced::Alignment::Center)
+               .into()
      }
 }
 
-
-
-fn main() {
-    println!("Hello, world!");
-
-    let x = 5;
-    let y = &x;
-
-    println!("x is: {}", x);
-    println!("y is: {}", y);
-
-    let mut z = 10;
-    let w = &mut z;
-    *w += 1;
-
-    println!("w is now: {}", w);
-    println!("z is now: {}", z);
-
-    let mut counter = Counter { value: 5};
- 
-    counter.update(Message::Increment);
-    counter.update(Message::Increment);
-    counter.update(Message::Decrement);
-
-    println!("counter is: {}", counter.value);
-
-    
-    let increment = button("+");
-    let decrement = button("-");
-    let counter = text(15);
-    let interface = column![increment, counter, decrement];
+fn main() -> iced::Result {
+    Counter::run(Settings::default())
 }
