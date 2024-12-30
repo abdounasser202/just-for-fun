5ff332252a66704352e7ee80e7fab1c08ac3dd82
Nasser <abdounasser202@gmail.com>
2024-12-30 21:58:08 +0100

    emacs: add markdown support

--- my_emacs_config.el
+++ my_emacs_config.el
@@ -50,6 +50,13 @@
 ;; Set up visible bell
 (setq visible-bell t)
 
+(when window-system
+  (set-frame-size (selected-frame) 175 65))
+
+;; To make it apply to new frames as well:
+(add-to-list 'default-frame-alist '(width . 175))
+(add-to-list 'default-frame-alist '(height . 65))
+
 ;; ================================================================================================;;
 
 ;; Duplicate the current line and place cursor on the new line.
@@ -151,10 +158,18 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(treemacs-magit magit doom-modeline doom-themes treemacs which-key counsel ivy use-package)))
+   '(markdown-mode treemacs-magit magit doom-modeline doom-themes treemacs which-key counsel ivy use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; Markdown Mode
+(use-package markdown-mode
+  :ensure t
+  :mode ("README\\.md\\'" . gfm-mode)
+  :init (setq markdown-command "pandoc")
+  :bind (:map markdown-mode-map
+         ("C-c C-e" . markdown-do)))
